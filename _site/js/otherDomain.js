4685d047b10b688ce16a0044cef87a737012fb9b
GuyZwer <guy.zwerdling@gmail.com>
2018-03-14 20:33:30 +0200

    update js folder

---
+++ _site/js/otherDomain.js
@@ -0,0 +1,6 @@
+function myFunction4(){
+document.getElementById("header").innerHTML = "JavaScript was run from otherDomain.js";
+document.getElementById("button").onclick = "myFunction5()";
+document.getElementById("button").innerHTML = "run script from otherFolder.js";
+}
+
--- _site/outherDomain.js
+++
@@ -1,6 +0,0 @@
-function myFunction4(){
-document.getElementById("header").innerHTML = "JavaScript was run from outherDomain.js";
-document.getElementById("button").onclick = "myFunction5()";
-document.getElementById("button").innerHTML = "run script from outherFolder.js";
-}
-
