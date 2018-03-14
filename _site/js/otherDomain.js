afc0d11bd4adf44167534541f25864f32bd2108b
GuyZwer <guy.zwerdling@gmail.com>
2018-03-14 21:11:32 +0200

    update javascript file

--- _site/js/otherDomain.js
+++ _site/js/otherDomain.js
@@ -1,5 +1,5 @@
 function myFunction4(){
 document.getElementById("header").innerHTML = "JavaScript was run from otherDomain.js";
-document.getElementById("button").onclick = "myFunction5()";
+document.getElementById("button").onclick = myFunction5;
 document.getElementById("button").innerHTML = "run script from otherFolder.js";
 }
