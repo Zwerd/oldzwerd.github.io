d91f238f9944f66378aaf99902524a2dc5ff1f97
GuyZwer <guy.zwerdling@gmail.com>
2018-05-04 12:05:24 +0300

    update the quiz

---
+++ _site/scripts/quiz.js
@@ -0,0 +1,15 @@
+function check() {
+  var answer = document.getElementById('answer')
+  var q1 = document.quiz.q1.value
+  var q2 = document.quiz.q2.value
+
+  var correct = 0
+
+  if (q1 == '1') {
+    correct++
+  }
+  if (q2 == '1') {
+    correct++
+  }
+  answer.innerHTML = 'Your scor is: ' + correct * 100 / 2
+}
